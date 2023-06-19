8c42d4c850d97d9a3fa32e5ed6258d7e40e446cb
Jonville <pjb1202@nate.com>
2023-06-19 11:16:04 +0900

    vite.confg.js 경로지정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 환경설정 - 소스들의 경로를 지정 / github 에서 assets 경로를 찾아줌
+  base: "./",
+
 })
+
